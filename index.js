65c62bbcd99cc54bdd375dbc2e0abc027579a0e3
Jean-David Caprace <jd.caprace@gmail.com>
2022-06-12 19:55:31 -0300

    new dependency

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * Add the MIT license
  */
 
-const startiridium = require('iridium-sbd')
+const startiridium = require('iridium-sbd.js')
 
 //To obtain the SignalK paths
 const signalkSchema = require('@signalk/signalk-schema')
